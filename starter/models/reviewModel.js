14b4426a770774c5bc4da18063e6a5303bae8ab5
nguyenhuyhoanganh <huyhoanganh142000@gmail.com>
2022-01-10 09:54:02 +0700

    create reviewModel.js

---
+++ starter/models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!']
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
+    },
+    createAt: {
+      type: Date,
+      // Date nhập vào định dạng yyyy-MM-dd
+      default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour!']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user!']
+    }
+  },
+  {
+    toJSON: { virtuals: true }, // virtual properties xuất ra dưới dạng JSON
+    toObject: { virtuals: true } // dữ liệu được xuất như 1 Object
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
