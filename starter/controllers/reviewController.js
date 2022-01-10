a968f646c5af30afcc8c8c74c8fbd4614b1e4f9e
nguyenhuyhoanganh <huyhoanganh142000@gmail.com>
2022-01-10 17:39:10 +0700

    Implementing Simple Nested Routes

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -1,18 +1,10 @@
 const Review = require('./../models/reviewModel');
-const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 // const AppError = require('./../utils/appError');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  // EXCUTE QUERY
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const reviews = await features.query;
+  const reviews = await Review.find();
 
-  // SEND RESPONSE
   res.status(200).json({
     status: 'success',
     results: reviews.length,
@@ -21,6 +13,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
