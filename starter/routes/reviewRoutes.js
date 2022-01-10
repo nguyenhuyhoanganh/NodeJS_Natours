df78f536bb1933960c1a652323a0c4e560457670
nguyenhuyhoanganh <huyhoanganh142000@gmail.com>
2022-01-10 21:32:19 +0700

    merge param from parent router to child router

--- starter/routes/reviewRoutes.js
+++ starter/routes/reviewRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+// đưa option { mergeParams: true } vào trong .Router() giúp reviewRoutes có quyền truy cập vào params trong url ở tourRouter :tourId
+// theo mặc định mỗi bộ định tuyến chỉ có quyền truy cập đến param trong url cụ thể của nó
 
 router
   .route('/')
--- starter/routes/tourRoutes.js
+++ starter/routes/tourRoutes.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
+// const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
 //param middleware : chỉ thực thi khi url có id
 // router.param('id', tourController.checkID);
+
+/*
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
+*/
+// POST /tours/532jsdfy349/reviews
+// GET /tours/532jsdfy349/reviews
+router.use('/:tourId/reviews', reviewRouter);
+// match url trong tourRouter tới reviewRouter để sử dụng chung chức năng với url trong reviewRouter
 
 router
   .route('/top-5-tours')
@@ -27,11 +43,4 @@ router
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
 module.exports = router;
